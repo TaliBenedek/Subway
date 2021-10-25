277571c0d31092924abf4111903043cd2c80ad63
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-10-24 21:23:38 -0400

    Finished getConnectedStations test

--- src/test/java/SubwayLinesTest.java
+++ src/test/java/SubwayLinesTest.java
@@ -2,7 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.*;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -13,13 +13,18 @@ public class SubwayLinesTest
     public void getConnectedStations() throws IOException
     {
         //given
-        JsonToSubwayLines jsonConverter = mock(JsonToSubwayLines.class);
-        SubwayLines lines = mock(SubwayLines.class);
-        doReturn(lines).when(jsonConverter).readJsonAsMap();
-        //TODO doReturn for lines.entrySet
+        JsonToSubwayLines jsonConverter = new JsonToSubwayLines();
+        SubwayLines lines = jsonConverter.readJsonAsMap();
+        Map map = new HashMap<String, int[]>();
+        map.put("7 Express", new int[]{103, 54, 24, 23, 25});
+        map.put("6 Express", new int[]{29, 2, 467, 457, 1, 105, 92});
+        map.put("4", new int[]{29, 2, 467, 457, 1, 105, 92});
+        map.put("6", new int[]{29, 2, 467, 457, 1, 105, 92});
+        map.put("7", new int[]{25, 23, 190, 189});
+
         SubwayStations stations = mock(SubwayStations.class);
-        doReturn("1").when(stations).getObjectId("Astor Pl");
-        doReturn("23").when(stations).getObjectId("Mets - Willets Point");
+        doReturn(1).when(stations).getObjectId("Astor Pl");
+        doReturn(23).when(stations).getObjectId("Mets - Willets Point");
         doReturn("Bleecker St").when(stations).getName(457);
         doReturn("Union Sq - 14th St").when(stations).getName(105);
         doReturn("Junction Blvd").when(stations).getName(24);
