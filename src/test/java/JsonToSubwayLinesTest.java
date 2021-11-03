beaa44a9aa7cd5564490ed7d7633b3cee168cab8
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-11-02 21:33:47 -0400

    Fixed test to match new json file

--- src/test/java/JsonToSubwayLinesTest.java
+++ src/test/java/JsonToSubwayLinesTest.java
@@ -19,7 +19,7 @@ public class JsonToSubwayLinesTest
 
         //then
         assertEquals(Sets.newSet("A", "B", "C", "D", "E", "F", "G", "J",
-                "L", "M", "N", "Q", "R", "S", "W", "Z",
+                "L", "M", "N", "Q", "R", "W", "Z",
                 "7 Express", "6 Express", "1", "2",
                 "3", "4", "5", "6", "7"), lines.keySet());
         assertArrayEquals(new int[]{470, 197, 466, 204, 101, 95, 93, 103, 54, 24, 23, 25}, lines.get("7 Express"));
